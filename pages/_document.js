039c33f998008b0c17f2c5e4c211f5ce8bda3bc7
ervin-sungkono <ervinsung@gmail.com>
2023-02-28 23:05:23 +0700

    refactor code

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,36 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
+  const profile = {
+    url: "https://ervin-sungkono.vercel.app",
+    title: "Ervin Cahyadinata Sungkono",
+    description: "Hello! im known as Ervin Cahyadinata Sungkono, a student at Binus University who has a passion for Web Development and Web Design. This is my personal website where i put my past projects and designs.",
+    imagePath: "https://ervin-sungkono.vercel.app/images/preview-img.png"
+  }
   return (
     <Html lang="en">
-      <Head/>
+      <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" type="image/x-icon"/>
+
+                <meta name="title" content={profile.title}/>
+                <meta name="description" content={profile.description}/>
+                <meta name="image" content={profile.imagePath}/>
+
+                {/* Open Graph / Facebook */}
+                <meta property="og:type" content="website"/>
+                <meta property="og:url" content={profile.url}/>
+                <meta property="og:title" content={profile.title}/>
+                <meta property="og:description" content={profile.description}/>
+                <meta property="og:image" content={profile.imagePath}/>
+
+                {/* Twitter */}
+                <meta property="twitter:card" content="summary_large_image"/>
+                <meta property="twitter:url" content={profile.url}/>
+                <meta property="twitter:title" content={profile.title}/>
+                <meta property="twitter:description" content={profile.description}/>
+                <meta property="twitter:image" content={profile.imagePath}/>
+      </Head>
       <body>
         <Main />
         <NextScript />
