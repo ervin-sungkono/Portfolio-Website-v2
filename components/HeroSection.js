5635626ea716f7975fdaa5038f032b2f6736d187
ervin-sungkono <ervinsung@gmail.com>
2023-03-01 12:55:33 +0700

    image optimization

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -53,7 +53,7 @@ export default function HeroSection(){
                 ))}
             </div>
             <div className="relative">
-                <Image src={HeroImage} width={400} height={400} quality={100} priority={true} alt="Gambar Ervin" className="relative animate-float"/>
+                <Image src={HeroImage} width={400} height={400} quality={100} alt="Gambar Ervin" className="relative animate-float"/>
                 <Image src={Square} width={360} height={360} priority={true} className="-z-10 absolute rotate-12 top-0 right-2 animate-rotate scale-75" alt="-"/>
             </div>
             <div className={styles["arrow-btn"]} onClick={() => ScrollIntoView({id: 'about-section', block: 'center'})}>
