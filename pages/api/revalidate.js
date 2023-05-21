c122659ded9e1259a621b347c7da0dce0e0db6b2
ervin-sungkono <ervinsung@gmail.com>
2023-05-21 20:56:36 +0700

    fix revalidate issue

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,14 +1,17 @@
-const pages = ["/","/about","/dribbble","/github"]
+const pages = ["/","/about","/design","/project"]
 
 export default async function handler(req, res) {
-    if (req.query.secret !== process.env.SECRET_TOKEN) {
-      return res.status(401).json({ message: 'Invalid token' })
-    }
-  
-    try {
-      await Promise.all(pages.map(page => res.revalidate(page)))
-      return res.json({ revalidated: true })
-    } catch (err) {
-      return res.status(500).send('Error revalidating')
-    }
+  const date = Date.now()
+
+  if (req.query.secret !== process.env.SECRET_TOKEN) {
+    return res.status(401).json({ message: 'Invalid token' })
+  }
+
+  try {
+    await Promise.all(pages.map(page => res.revalidate(page)))
+    return res.json({ revalidated: true, timeTaken: `${Date.now() - date}ms` })
+  } catch (err) {
+    console.log(err)
+    return res.status(500).send('Error revalidating')
+  }
 }
