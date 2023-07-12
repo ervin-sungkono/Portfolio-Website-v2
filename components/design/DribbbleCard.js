bce206a806a1fc03641a2cd5194cb6a60205b749
ervin-sungkono <ervinsung@gmail.com>
2023-07-12 21:01:07 +0700

    update hover effect on dribbble card

--- components/design/DribbbleCard.js
+++ components/design/DribbbleCard.js
@@ -3,12 +3,13 @@ import ImageWithFallback from "../common/ImageWithFallback"
 
 export default function DribbbleCard({ shot }){
     return(
-        <Link href={shot.html_url} target="_blank">
+        <Link href={shot.html_url} target="_blank" className="group">
             <ImageWithFallback
                 src={shot.images.normal}
                 fallbackSrc={`https://raw.githubusercontent.com/ervin-sungkono/web-assets/master/images/no-image.png`}
                 fill
                 alt={shot.title}
+                className="group-hover:scale-[1.15] transition-transform duration-700 ease-in-out"
             />
             <div className="shot-overlay bg-black/[0.6] dark:bg-white/[0.7]">
                 <h3>{shot.title}</h3>
